289be00b49fc39d04f32e94b0e31f3be1242a559
jangdong-hun <33648003+jangdong-hun@users.noreply.github.com>
2018-05-31 01:06:35 +0900

    na il hal rae

--- Knight.java
+++ Knight.java
@@ -1,8 +1,19 @@
-
+/*    y
+ * x 00
+ *
+ *         88
+ * */
 public class Knight extends ChessPieces {
 	public Knight(Position curPosition, char color) {
 		this.curPosition = curPosition;
 		this.color = color;
+		this.isAlive=true;
 	}
-	
+	public ChessBoardCell[] isMovable(Position curPosition, ChessBoardCell[][] cells, char color) {
+		int num=0;
+		ChessBoardCell[] availCells = new ChessBoardCell[8];
+		if (color == 'B') {// Black
+			if ((cells[curPosition.x + 2][curPosition.y - 1].piece == null)||(cells[curPosition.x + 1][curPosition.y].piece.color=='W'))
+		}
+	}
 }
--- Pawn.java
+++ Pawn.java
@@ -7,12 +7,12 @@ public class Pawn extends ChessPieces {
 	public Pawn(Position curPosition, char color) {
 		this.curPosition = curPosition;
 		this.color = color;
+		this.isAlive=true;
 	}
 
 	public boolean first = true;
 
 	public ChessBoardCell[] isMovable(Position curPosition, ChessBoardCell[][] cells, char color) {//능력의 한계로 폰이 한 번에 가질 수 있는 최대의 경우의 수 4개의 cells의 배열을 반환 2개면 cells[2]부턴 null
-		                                                                                           //생각을 해보니 8X8을 벗어날때 생각을 안했다 흐하하하하하하하하  
 		int num=0;
 		ChessBoardCell[] availCells = new ChessBoardCell[4];
 		if (color == 'B') {// Black
